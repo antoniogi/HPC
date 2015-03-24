ab7b878cc213d04c7ef897d39071cd7b8248dada
Antonio Gomez <agomez@tacc.utexas.edu>
2015-03-24 08:25:21 -0500

    Example of how to use the local disk on Stampede

---
+++ slurm/use_local_filesystem/task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Retrieve my rank
+rank=$(( ${PMI_RANK-0} + ${PMI_ID-0} + ${MPIRUN_RANK-0} + ${OMPI_COMM_WORLD_RANK-0} + ${OMPI_MCA_ns_nds_vpid-0} ))
+echo $rank
+
+# Create a folder in the local disk
+mkdir /tmp/$rank
+
+cp my_files /tmp/$rank/
+
+#run_your_code_here using files in /tmp/$rank
+
+./mycode /tmp/$rank/input_file
+
+#if you need to move some of your files back
+# cp /tmp/$rank/output_file output_file.$rank
